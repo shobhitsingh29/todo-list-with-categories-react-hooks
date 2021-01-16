b4d5ceb0ae751cc03b8717bf5466a3cd4273a07a
shobhit.singh <shobhit.singh@flipkart.com>
2021-01-16 18:22:57 +0530

    Updates

---
+++ precache-manifest.b5946e02a133aab4be6c1907649613ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1498016132c9f7c474daf36917a534c5",
+    "url": "/todo-list-with-categories-react-hooks/index.html"
+  },
+  {
+    "revision": "4d7f82d9494620149443",
+    "url": "/todo-list-with-categories-react-hooks/static/css/main.ef8b9f35.chunk.css"
+  },
+  {
+    "revision": "fd9755b289cba43742d0",
+    "url": "/todo-list-with-categories-react-hooks/static/js/2.9c0ccfdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-with-categories-react-hooks/static/js/2.9c0ccfdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d7f82d9494620149443",
+    "url": "/todo-list-with-categories-react-hooks/static/js/main.19e5d335.chunk.js"
+  },
+  {
+    "revision": "aba483882d33900a29a1",
+    "url": "/todo-list-with-categories-react-hooks/static/js/runtime-main.5976fd90.js"
+  }
+]);
